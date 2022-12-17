71e40dd6907b1a00445470dbe2ec33d7bc5ca658
Rishi Goswami <92781956+rishig2003@users.noreply.github.com>
2022-12-17 11:53:47 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+    fontFamily:{
+      'Lemon': ['Lemon', 'sans-serif'],
+      'DynaPuff':['DynaPuff','sans-serif'],
+
+    },
+  },
+  plugins: [],
+}
